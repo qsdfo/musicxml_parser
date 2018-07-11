312ea78e4497e4206034de104b91efb4d84a0813
Leo Crestel Mac <crestel.leopold@gmail.com>
2018-07-11 12:22:01 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='musicxml_parser',
+      version='0.1',
+      description='A musicXML parser written in Python based on a SAX analyzer',
+      url='https://github.com/qsdfo/musicxml_parser',
+      author='Léopold Crestel',
+      author_email='crestel.leopold@gmail.com',
+      license='MIT',
+      packages=['musicxml_parser'],
+      zip_safe=False)
